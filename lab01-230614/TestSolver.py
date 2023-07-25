6735d0fad88f4c9462574ddb593ec37b5b8d0234
Viktor Grischenko <viktor.grischenko@ax-team.com>
2023-07-25 22:51:07 +0300

    [lab01] Step 7. Double-the-same root test passes

--- lab01-230614/TestSolver.py
+++ lab01-230614/TestSolver.py
@@ -21,6 +21,16 @@ class TestSolver(unittest.TestCase):
         self.assertAlmostEqual(res[0], -1);
         self.assertAlmostEqual(res[1], 1);
 
+
+    # Step 7. Double-the-same roots test
+    def test_double_roots(self):
+        res = self.solver.sqrSolve(1, 2, 1);
+
+        self.assertEqual(len(res), 2);
+        self.assertAlmostEqual(res[0], -1);
+        self.assertAlmostEqual(res[1], -1);
+
+
        
 
 # Executing the tests in the above test case class
