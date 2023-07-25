32e3eb0c7d47f81bc470caae355e3ec5023855c8
Viktor Grischenko <viktor.grischenko@ax-team.com>
2023-07-25 23:14:23 +0300

    [lab01] code cleaning

--- lab01-230614/TestSolver.py
+++ lab01-230614/TestSolver.py
@@ -5,30 +5,30 @@ from Solver import Solver
 class TestSolver(unittest.TestCase):
     # Pre-test routines
     def setUp(self):
-        self.solver = Solver();
+        self.solver = Solver()
 
 
     # Step 3. No roots test
     def test_no_roots(self):
-        res = self.solver.sqrSolve(1, 0, 1);
-        self.assertEqual(len(res), 0);
+        res = self.solver.sqrSolve(1, 0, 1)
+        self.assertEqual(len(res), 0)
 
     # Step 5. Two roots test
     def test_two_roots(self):
-        res = self.solver.sqrSolve(1, 0, -1);
+        res = self.solver.sqrSolve(1, 0, -1)
 
-        self.assertEqual(len(res), 2);
-        self.assertAlmostEqual(res[0], -1);
-        self.assertAlmostEqual(res[1], 1);
+        self.assertEqual(len(res), 2)
+        self.assertAlmostEqual(res[0], -1)
+        self.assertAlmostEqual(res[1], 1)
 
 
     # Step 7. Double-the-same roots test
     def test_double_roots(self):
-        res = self.solver.sqrSolve(1, 2, 1);
+        res = self.solver.sqrSolve(1, 2, 1)
 
-        self.assertEqual(len(res), 2);
-        self.assertAlmostEqual(res[0], -1);
-        self.assertAlmostEqual(res[1], -1);
+        self.assertEqual(len(res), 2)
+        self.assertAlmostEqual(res[0], -1)
+        self.assertAlmostEqual(res[1], -1)
 
     # Step 9. Zero "a" test
     def test_zero_a(self):
